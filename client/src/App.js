8120f046436e7db1eea6f04d163e56012e1d3e2b
Maksim <maksim_belotelov@mail.ru>
2025-01-13 20:11:33 +0300

    02: added base table structure and Bootstrap styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className='card-header'>
+          <h1>Список напоминаний</h1>
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>Id</th>
+                <th scope='col'>Title</th>
+                <th scope='col'>Напоминание</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <th scope='col'>Заголовок 1</th>
+                <th scope='col'>01.01.2026 12:00</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
